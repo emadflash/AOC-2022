099cc118b4eb984d6bc390eaa9f90a08a72d2783
madflash <backbugkaught@gmail.com>
2022-12-23 18:44:25 +0530

    solve day 10 part 2

--- Day10/src/main.rs
+++ Day10/src/main.rs
@@ -4,10 +4,43 @@ use std::error::Error;
 
 const INPUT: &str = include_str!("../input.txt");
 
+struct Crt {
+    crt: [[char; 40]; 6],
+    col: usize,
+    row: usize,
+}
+
+impl Crt {
+    fn new() -> Self {
+        Self {
+            crt: [['.'; 40]; 6],
+            col: 0,
+            row: 0,
+        }
+    }
+
+    fn draw(&mut self, reg_x: &i32) {
+        if self.col == 40 {
+            self.col = 0;
+            self.row += 1;
+        }
+
+        let col = self.col as i32;
+        let (lo, hi) = (reg_x - 1, reg_x + 1);
+
+        if col >= lo && col <= hi {
+            self.crt[self.row][self.col] = '█';
+        }
+        self.col += 1;
+    }
+}
+
 struct Cpu {
     reg_x: i32,
     cycles: i32,
     signal_strength: i32,
+    crt: Crt,
+    next_epoch_cycle: u16,
 }
 
 impl Cpu {
@@ -16,23 +49,21 @@ impl Cpu {
             reg_x: 1,
             cycles: 1,
             signal_strength: 0,
+            crt: Crt::new(),
+            next_epoch_cycle: 20,
         }
     }
 
-    #[inline]
-    fn is_next_40th_cycle(&self) -> bool {
-        self.cycles == 20
-            || self.cycles == 60
-            || self.cycles == 100
-            || self.cycles == 140
-            || self.cycles == 180
-            || self.cycles == 220
+    fn update_signal_strength(&mut self) {
+        if self.cycles == self.next_epoch_cycle as i32 {
+            self.signal_strength += self.reg_x * self.cycles;
+            self.next_epoch_cycle += 40;
+        }
     }
 
     fn tick(&mut self, asm: &str) {
-        if self.is_next_40th_cycle() {
-            self.signal_strength += self.reg_x * self.cycles;
-        }
+        self.update_signal_strength();
+        self.crt.draw(&self.reg_x);
 
         match asm.chars().next() {
             Some('n') => self.cycles += 1,
@@ -41,9 +72,8 @@ impl Cpu {
                 let operand = operand.parse::<i32>().unwrap();
                 self.cycles += 1;
 
-                if self.is_next_40th_cycle() {
-                    self.signal_strength += self.reg_x * self.cycles;
-                }
+                self.update_signal_strength();
+                self.crt.draw(&self.reg_x);
 
                 self.reg_x += operand;
                 self.cycles += 1;
@@ -63,13 +93,31 @@ fn part1(input: &str) -> i32 {
     cpu.signal_strength
 }
 
-fn part2(input: &str) -> u32 {
-    todo!();
+fn part2(input: &str) {
+    let mut cpu = Cpu::new();
+    for asm in input.split("\n").filter(|l| !l.is_empty()) {
+        cpu.tick(asm);
+    }
+
+    for l in cpu.crt.crt {
+        println!("{:?}", l.iter().collect::<String>());
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("part 1: {}", part1(INPUT));
-    println!("part 2: {}", part2(INPUT));
+    println!("part 2: ");
+    part2(INPUT);
 
+    assert_eq!(part1(INPUT), 15880);
     Ok(())
 }
+
+// Part 2 answer, cool af :)
+//
+// "███..█.....██..████.█..█..██..████..██.."
+// "█..█.█....█..█.█....█.█..█..█....█.█..█."
+// "█..█.█....█....███..██...█..█...█..█...."
+// "███..█....█.██.█....█.█..████..█...█.██."
+// "█....█....█..█.█....█.█..█..█.█....█..█."
+// "█....████..███.█....█..█.█..█.████..███."
